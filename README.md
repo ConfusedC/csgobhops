b1b97836e66aa09120562e4c4c8d6d760a9f8622
Pratyush <pthapli999@gmail.com>
2020-04-25 22:15:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# csgobhops" 
---
+++ bhop.py
@@ -0,0 +1,30 @@
+from win32gui import GetWindowText, GetForegroundWindow
+import time
+import keyboard
+import pymem
+
+fj = (0x51ED750)
+lp = (0xD2FB84)
+f = (0x104)
+
+
+
+pm = pymem.Pymem("csgo.exe")
+client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll").lpBaseOfDll
+
+while True:
+    if not GetWindowText(GetForegroundWindow()) == "Counter-Strike: Global Offensive":
+        continue
+
+    if keyboard.is_pressed("space"):
+        forceJump = client + fj
+        player = pm.read_int(client + lp)
+        if player:
+            on_ground = pm.read_int(player + f)
+            if on_ground and on_ground == 257:
+                pm.write_int(forceJump, 5)
+                time.sleep(0.08)
+                pm.write_int(forceJump, 4)
+
+    time.sleep(0.002)
+
